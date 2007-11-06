5c2ec695bea1cb4027ba46616d66815968ca2f96
7rans <transfire@gmail.com>
2007-11-06 18:55:01 +0000

    Updated extconf.rb files to copy extension to arch directories.

--- ext/tmail/scanner_c/extconf.rb
+++ ext/tmail/scanner_c/extconf.rb
@@ -4,4 +4,9 @@ extension_name = 'scanner_c'
 
 dir_config(extension_name)
 
-create_makefile(extension_name)
+#create_makefile(extension_name, 'tmail')
+
+# Will copy to arch subdir.
+require 'rbconfig'
+create_makefile(extension_name, "tmail/#{Config::CONFIG['arch']}")
+
