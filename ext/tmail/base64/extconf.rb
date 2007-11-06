511db802c2928c63a5a6292bee58a5a490d2328c
7rans <transfire@gmail.com>
2007-11-06 14:24:57 +0000

    Added config_dir setting to extconf.rb (although it dodn't seem to do any good).

--- ext/tmail/base64/extconf.rb
+++ ext/tmail/base64/extconf.rb
@@ -1,2 +1,7 @@
 require 'mkmf'
-create_makefile 'base64'
+
+extension_name = 'base64'
+
+dir_config(extension_name)
+
+create_makefile(extension_name)
--- ext/tmail/scanner_c/extconf.rb
+++ ext/tmail/scanner_c/extconf.rb
@@ -1,2 +1,7 @@
 require 'mkmf'
-create_makefile 'scanner_c'
+
+extension_name = 'scanner_c'
+
+dir_config(extension_name)
+
+create_makefile(extension_name)
