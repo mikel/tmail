378ffdd8fd89605cfcb8113ac404ce6726e27caf
7rans <transfire@gmail.com>
2007-11-06 14:37:40 +0000

    Added require_arch.rb.

---
+++ lib/tmail/require_arch.rb
@@ -0,0 +1,41 @@
+require 'rbconfig'
+
+# Attempts to require anative extension.
+# Fallsback to pure-ruby versoin, if it fails.
+#
+# This uses Config::CONFIG['arch'] and Config::CONFIG['DLEXT']
+# from rbconfig.
+
+def require_arch(fname)
+  dext = Config::CONFIG['DLEXT']
+  begin
+    if File.extname(fname) == dext
+      path = fname
+    else
+      path = File.join("tmail","#{fname}.#{dext}")
+    end
+    require path
+  rescue LoadError => e
+    begin
+      arch = Config::CONFIG['arch']
+      path = File.join("tmail", arch, "#{fname}.#{dext}")
+      require path
+    rescue LoadError
+      case path
+      when /i686/
+        path.sub!('i686', 'i586')
+      when /i586/
+        path.sub!('i586', 'i486')
+      when /i486/
+        path.sub!('i486', 'i386')
+      else
+        begin
+          require fname + '.rb'
+        rescue LoadError
+          raise e
+        end
+      end
+      retry
+    end
+  end
+end
