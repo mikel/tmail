b02cec410c8a73b6933483c1b487e8c73262e3bf
7rans <transfire@gmail.com>
2007-11-07 17:30:36 +0000

    Finally! Finally! Got Windows to install.

--- ext/tmail/base64/extconf.rb
+++ ext/tmail/base64/extconf.rb
@@ -5,17 +5,31 @@ extension_name = 'base64'
 
 arch = Config::CONFIG['arch']
 
+windows = (/mswin/ =~ arch) #RUBY_PLATFORM)
+
 FailedMessage.replace("Could not create Makefile, probably for the lack of necessary libraries and/or headers. Check the mkmf.log file for more details. You may need configuration options (see below). TMail has a pure-ruby fallback mode, so you can still use this library. To do so, set the environment variable, export NORUBYEXT='true', and gem install again.\n\n")
 
-if ENV['NORUBYEXT'] == 'true' || arch =~ /mswin/
+if (ENV['NORUBYEXT'] == 'true') || windows  # TEMPORARILY ADD WINDOWS HERE
   # Rubygems is sending all output to dev/null :(
-  STDOUT << "Native extension will be omitted."
+  #STDOUT << "Native extension will be omitted."
+  #ENV['make'] = 'echo' # THIS DOESN"T GET TO THE PARENT PROCESS!!!
+
+  if windows
+    # LETS TRY FAKING IT OUT.
+    File.open('make.bat', 'w') do |f|
+      f << 'echo Native extension will be omitted.'
+    end
+    File.open('nmake.bat', 'w') do |f|
+      f << 'echo Native extension will be omitted.'
+    end
+    File.chmod(0755, "make.bat", "nmake.bat")  # need?
+  end
   File.open('Makefile', 'w') do |f|
     f << "all:\n"
     f << "install:\n"
   end
 else
-  if (/mswin/ =~ RUBY_PLATFORM) and ENV['make'].nil?
+  if windows && ENV['make'].nil?
     $LIBS += " msvcprt.lib"
     #dir_config(extension_name)
     #create_makefile(extension_name, "tmail")
