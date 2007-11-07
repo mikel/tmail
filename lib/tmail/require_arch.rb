c76be807985c596a0dc97d4a6d21725b1cc219db
7rans <transfire@gmail.com>
2007-11-07 11:49:24 +0000

    Updated require_arch and removed linux binaries.

--- lib/tmail/require_arch.rb
+++ lib/tmail/require_arch.rb
@@ -7,35 +7,48 @@ require 'rbconfig'
 # from rbconfig.
 
 def require_arch(fname)
-  dext = Config::CONFIG['DLEXT']
+  #dext = Config::CONFIG['DLEXT']
   begin
-    if File.extname(fname) == dext
-      path = fname
-    else
-      path = File.join("tmail","#{fname}.#{dext}")
-    end
+    arch = Config::CONFIG['arch']
+    #path = File.join("tmail", arch, "#{fname}.#{dext}")
+    path = File.join("tmail", arch, fname)
     require path
-  rescue LoadError => e
-    begin
-      arch = Config::CONFIG['arch']
-      path = File.join("tmail", arch, "#{fname}.#{dext}")
-      require path
-    rescue LoadError
-      case path
-      when /i686/
-        path.sub!('i686', 'i586')
-      when /i586/
-        path.sub!('i586', 'i486')
-      when /i486/
-        path.sub!('i486', 'i386')
-      else
-        begin
-          require fname + '.rb'
-        rescue LoadError
-          raise e
-        end
-      end
-      retry
-    end
+  rescue LoadError
+    require fname
   end
 end
+
+
+# def require_arch(fname)
+#   dext = Config::CONFIG['DLEXT']
+#   begin
+#     if File.extname(fname) == dext
+#       path = fname
+#     else
+#       path = File.join("tmail","#{fname}.#{dext}")
+#     end
+#     require path
+#   rescue LoadError => e
+#     begin
+#       arch = Config::CONFIG['arch']
+#       path = File.join("tmail", arch, "#{fname}.#{dext}")
+#       require path
+#     rescue LoadError
+#       case path
+#       when /i686/
+#         path.sub!('i686', 'i586')
+#       when /i586/
+#         path.sub!('i586', 'i486')
+#       when /i486/
+#         path.sub!('i486', 'i386')
+#       else
+#         begin
+#           require fname + '.rb'
+#         rescue LoadError
+#           raise e
+#         end
+#       end
+#       retry
+#     end
+#   end
+# end
