3fa316f6408bf37ceb1050aa82285c2d2d4fb00d
7rans <transfire@gmail.com>
2007-11-22 16:59:04 +0000

    Remove base64 c extension, as it's speed benefit was negligable.

--- lib/tmail/base64.rb
+++ lib/tmail/base64.rb
@@ -1,73 +1,52 @@
-# = Base64 handling class
+# = TITLE:
 #
-# Copyright (c) 1998-2003 Minero Aoki <aamine@loveruby.net>
+#   Base64
 #
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
+# = COPYRIGHT:
 #
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
+#   Copyright (c) 1998-2003 Minero Aoki <aamine@loveruby.net>
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#   Permission is hereby granted, free of charge, to any person obtaining
+#   a copy of this software and associated documentation files (the
+#   "Software"), to deal in the Software without restriction, including
+#   without limitation the rights to use, copy, modify, merge, publish,
+#   distribute, sublicense, and/or sell copies of the Software, and to
+#   permit persons to whom the Software is furnished to do so, subject to
+#   the following conditions:
 #
-# Note: Originally licensed under LGPL v2+. Using MIT license for Rails
-# with permission of Minero Aoki.
+#   The above copyright notice and this permission notice shall be
+#   included in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+#   EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+#   NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+#   LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+#   OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+#   WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#   Note: Originally licensed under LGPL v2+. Using MIT license for Rails
+#   with permission of Minero Aoki.
 
-begin
-  raise LoadError, 'Turned off native extentions by user choice' if ENV['NORUBYEXT']
-  require 'tmail/base64_c'
-rescue LoadError
-  require 'tmail/base64_r'
+#
+module TMail
+
+  module Base64
+
+    module_function
+
+    def folding_encode( str, eol = "\n", limit = 60 )
+      [str].pack('m')
+    end
+
+    def encode( str )
+      [str].pack('m').tr( "\r\n", '' )
+    end
+
+    def decode( str, strict = false )
+      str.unpack('m').first
+    end
+
+  end
+
 end
-
-# module Base64
-#
-#   def self.folding_encode( str, eol = "\n", limit = 60 )
-#     b64encode( str, limit )
-#   end
-#
-#   def self.encode( str )
-#     encode64( str )
-#   end
-#
-#   def self.decode( str, strict = false )
-#     if strict
-#       decode_b( str )
-#     else
-#       decode64( str )
-#     end
-#   end
-#
-#   #begin
-#   #  require('base64.so')  # TODO technically this is bad b/c of .dll (?)
-#   #  alias folding_encode c_folding_encode
-#   #  alias encode         c_encode
-#   #  alias decode         c_decode
-#   #  class << self
-#   #    alias folding_encode c_folding_encode
-#   #    alias encode         c_encode
-#   #    alias decode         c_decode
-#   #  end
-#   #rescue LoadError
-#   #  alias folding_encode rb_folding_encode
-#   #  alias encode         rb_encode
-#   #  alias decode         rb_decode
-#   #  class << self
-#   #    alias folding_encode rb_folding_encode
-#   #    alias encode         rb_encode
-#   #    alias decode         rb_decode
-#   #  end
-#   #end
-#
-# end
