cf0ad0e873244f8d78273f47efe669c0702c8c4f
mikel <raasdnil@gmail.com>
2007-12-25 10:00:43 +0000

    Included a require 'stringio' to hack resolve a load order problem when running autotest

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,6 @@
 # Add in the local library for the load path so we get that before any possible
 # gem that is already installed.
+require 'stringio'
 $:.unshift File.dirname(__FILE__) + "/../lib"
 $:.unshift File.dirname(__FILE__) + "/../lib/tmail"
 require 'test/unit'
